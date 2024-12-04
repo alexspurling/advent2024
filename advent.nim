f853b4187e5121b33666c163a32a0fe48ba9b68b
Alex Spurling <alexspurling@gmail.com>
2024-12-04 13:15:51 +0000

    Day 4

--- advent.nim
+++ advent.nim
@@ -1,5 +1,5 @@
 import strformat
-import solutions / [day1, day2, day3]
+import solutions / [day1, day2, day3, day4]
 
 proc solve(day: int, part: int): int {.exportc.} =
   case fmt"{day} - {part}":
@@ -9,6 +9,8 @@ proc solve(day: int, part: int): int {.exportc.} =
     of "2 - 2": return day2.solvePart2()
     of "3 - 1": return day3.solvePart1()
     of "3 - 2": return day3.solvePart2()
+    of "4 - 1": return day4.solvePart1()
+    of "4 - 2": return day4.solvePart2()
     else: return -1
 
-echo solve(3, 2)
+echo solve(4, 2)
